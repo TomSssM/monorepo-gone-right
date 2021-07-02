f44fe9689c0468467a411fb5eea139aceae2e1b9
TomSssM <ilyashome3@gmail.com>
2021-07-02 15:30:05 +0300

    test package

--- services/metrika/shared/index.ts
+++ services/metrika/shared/index.ts
@@ -1,6 +1,11 @@
 import { Car } from '@tomsssm/ui/shared';
+import { Test } from '@tomsssm/test/Test';
 
 const c = new Car();
+
+const t = new Test('dude');
+
+console.log(t.name);
 
 c.drive(890).then(() => {
     console.log('shared');
